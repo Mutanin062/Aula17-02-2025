e1421b46c35a06eb3bbf2d5ad5a869c8acfb6753
Mutanin062 <166817435+Mutanin062@users.noreply.github.com>
2025-02-17 09:41:18 -0300

    Create Contador.jsx

---
+++ Contador.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+
+function Contador(){
+
+    const [count, setCount] = useState(0);
+
+    return (
+        <div>
+            <p>Contador: {count}</p>
+            <button onClick={()=> setCount(count + 1)}>Incrementar</button>
+        </div>    
+
+    );
+}
+
+export default Contador;
