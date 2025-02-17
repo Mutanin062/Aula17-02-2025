442f62dc016d0b733cb059a905bff1e5e4c974ec
Mutanin062 <166817435+Mutanin062@users.noreply.github.com>
2025-02-17 09:40:26 -0300

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,13 @@
+import HelloWorld from "./HelloWorld";
+import Contador from "./contador";
+import Mensagem from "./Mensagem";
+function App(){
+  return(
+    <div>
+         <HelloWorld />
+         <Contador />
+         <Mensagem />
+    </div>     
+  );
+}
+export default App;
